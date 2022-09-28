f3792e7d4d3aa0df64c3168fcf86ae2789c21365
tjehdgur1500 <tjehdgur1500@naver.com>
2022-09-28 23:14:35 +0900

    init & contract writing

---
+++ contracts/Token.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.17;
+
+contract Token{
+    string public name = "My Hardhat Token";
+    string public symbol = "MTT";
+
+    uint256 public totalSupply = 1000000;
+}
